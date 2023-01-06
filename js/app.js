49b2b5197c41d6be5cc275eed39501df61e0276d
jugosack <jugoslav_ackoski@yahoo.com>
2023-01-06 23:24:59 +0100

    Fixing media queris

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ const speakers = [
     presenter: 'Jacob Bronowski',
     title: 'Polish-British mathematician and philosopher.',
     description:
-      'known to friends and professional colleagues alike by the nickname Bruno. He is best known for developing a humanistic approach to science, and as the presenter and writer of the thirteen-part 1973 BBC television documentary series, and accompanying book, The Ascent of Man.',
+      'He is an experienced pilot , entrepreneur, Burning Circle airshow sponsor , flight instructor an Burning Circle Flight Academy.',
     img: 'images/desktop/speaker_03.png',
   },
   {
@@ -54,7 +54,7 @@ const speakers = [
     title:
       'English theoretical physicist, cosmologist, and author of The Centre for Theoretical Cosmology at the University of Cambridge',
     description:
-      "Hawking's scientific works included a collaboration with Roger Penrose on gravitational singularity theorems in the framework of general relativity, and the theoretical prediction that black holes emit radiation, often called Hawking radiation. ",
+      'He is an experienced pilot , entrepreneur, Burning Circle airshow sponsor , flight instructor an Burning Circle Flight Academy.',
     img: 'images/desktop/speaker_04.png',
   },
   {
@@ -62,7 +62,7 @@ const speakers = [
     presenter: 'Richard Dawkins',
     title: 'British evolutionary biologist and author.',
     description:
-      'Dawkins first came to prominence with his 1976 book The Selfish Gene, which popularised the gene-centred view of evolution and introduced the term meme. With his book The Extended Phenotype (1982), he introduced into evolutionary biology the influential concept that the phenotypic effects.',
+      'He is an experienced pilot , entrepreneur, Burning Circle airshow sponsor , flight instructor an Burning Circle Flight Academy.',
     img: 'images/desktop/speaker_05.png',
   },
   {
@@ -71,7 +71,7 @@ const speakers = [
     title:
       'American paleontologist, evolutionary biologist, and historian of science.',
     description:
-      'He was one of the most influential and widely read authors of popular science of his generation.[1] Gould spent most of his career teaching at Harvard University and working at the American Museum of Natural History in New York.',
+      'He is an experienced pilot , entrepreneur, Burning Circle airshow sponsor , flight instructor an Burning Circle Flight Academy.',
     img: 'images/desktop/speaker_06.png',
   },
 ];
